79f06862f879f1a490a8ceb6c8a9b546bd304472
akelly <alexandrakelly@alexandras-macbook-air-5.local>
2017-02-08 11:04:03 -0600

    Created custom Class

---
+++ College Profile Builder/Colleges.swift
@@ -0,0 +1,24 @@
+//
+//  Colleges.swift
+//  College Profile Builder
+//
+//  Created by Alexandra Kelly  on 2/8/17.
+//  Copyright © 2017 Alexandra Kelly . All rights reserved.
+//
+
+import UIKit
+
+class Colleges: NSObject {
+    var name = String()
+    var location = String()
+    var enrollment = Int()
+    var image = Data()
+    
+    convenience init(name:String, location: String, enrollment: Int, image: Data){
+    self.init()
+    self.name = name
+    self.location = location
+    self.enrollment = enrollment
+    self.image = image
+    }
+}
